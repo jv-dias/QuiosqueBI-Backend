fb6cb12abddaf8bf584e2a533b9b8cf6386fdda3
João Victor Dias <jvictor.dev@outlook.com>
2025-07-12 15:43:53 -0300

    feat: Atualizar configuração de JWT para exigir HTTPS em produção

--- QuiosqueBI.API/Program.cs
+++ QuiosqueBI.API/Program.cs
@@ -51,7 +51,7 @@ builder.Services.AddAuthentication(options =>
     .AddJwtBearer(options =>
     {
         options.SaveToken = true;
-        options.RequireHttpsMetadata = false;
+        options.RequireHttpsMetadata = true; // Em produção, deve ser true.
         options.TokenValidationParameters = new TokenValidationParameters()
         {
             ValidateIssuer = true,
